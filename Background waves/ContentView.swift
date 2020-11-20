552ae26768f89c4eed11aaacd1648600acf9b923
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-11-19 19:36:10 -0500

    added 3 waves (call the function and fill parameters and animate)

--- Background waves/ContentView.swift
+++ Background waves/ContentView.swift
@@ -3,7 +3,7 @@
 //  Background waves
 //
 //  Created by Cloutier, Vincent on 2020-11-19.
-//
+//https://www.youtube.com/watch?v=iBgQRpIzY9g
 
 import SwiftUI
 
@@ -14,8 +14,31 @@ struct ContentView: View {
     @State var isAnimated = false
 
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            getSinWave(interval: universalSize.width, amplitude: 200, baseline: -50 + universalSize.height/2)
+                .foregroundColor(Color.init(red: 0.3, green: 0.6, blue: 1).opacity(0.4))
+                .offset(x: isAnimated ? -1*universalSize.width : 0)
+            .animation(
+                Animation.linear(duration: 2)
+                .repeatForever(autoreverses: false)
+            )
+            getSinWave(interval: universalSize.width*1.2, amplitude: 150, baseline: 50 + universalSize.height/2)
+                .foregroundColor(Color.init(red: 0.3, green: 0.6, blue: 1).opacity(0.4))
+                .offset(x: isAnimated ? -1*(universalSize.width*1.2) : 0)
+            .animation(
+                Animation.linear(duration: 5)
+                .repeatForever(autoreverses: false)
+            )
+            getSinWave(interval: universalSize.width*1.7, amplitude: 180, baseline: 50 + universalSize.height/2)
+                .foregroundColor(Color.init(red: 0.3, green: 0.9, blue: 1).opacity(0.4))
+                .offset(x: isAnimated ? -1*(universalSize.width*1.2) : 0)
+            .animation(
+                Animation.linear(duration: 1.9)
+                .repeatForever(autoreverses: false)
+            )
+        }.onAppear(){
+            self.isAnimated = true
+        }
     }
     func getSinWave(interval:CGFloat, amplitude: CGFloat = 100 , baseline : CGFloat = UIScreen.main.bounds.height/2) -> Path {
         Path{path in
