fc1c362c285826ab3bc9901c77a211e9b84d864c
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-11-19 19:26:35 -0500

    Initial Commit

---
+++ Background waves/Background_wavesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Background_wavesApp.swift
+//  Background waves
+//
+//  Created by Cloutier, Vincent on 2020-11-19.
+//
+
+import SwiftUI
+
+@main
+struct Background_wavesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Background waves/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Background waves
+//
+//  Created by Cloutier, Vincent on 2020-11-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
